8e935de58796b38ec87baa5373c6023f6b87cdb1
Sam33 <sup3355@gmail.com>
2020-06-07 11:21:29 +0800

    Add test UI

---
+++ SoCRM.py
@@ -0,0 +1,18 @@
+﻿from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtWidgets
+from SoCRM_form import Ui_Dialog
+import sys
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
